c58b149266af52a24a3e606a17ace3d4f869d85f
Johan Östling <kjostling@gmail.com>
2014-01-07 12:40:06 +0100

    new header

--- sites/all/themes/wappo/templates/page.tpl.php
+++ sites/all/themes/wappo/templates/page.tpl.php
@@ -73,52 +73,38 @@
  * @ingroup themeable
  */
 ?>
-<header>
-  
-  <div id="page-top" class="container">
-        <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-        <?php endif; ?>
-
-        <?php if (!empty($site_name)): ?>
-        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-        <?php endif; ?>
-		<?php if (!empty($page['navigation'])): ?>
-	  	<div class="top-links pull-right">
-             <?php print render($page['navigation']); ?>
-      	</div>
-		<?php endif; ?>
-  </div>
-  <div id="navbar-container">
-  <div id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-      <div class="navbar-header">
-
-        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      </div>
-
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-        <div class="navbar-collapse collapse">
-          <nav role="navigation">
-            <?php if (!empty($primary_nav)): ?>
-              <?php print render($primary_nav); ?>
-            <?php endif; ?>
-            <?php if (false && !empty($secondary_nav)): ?>
-              <?php print render($secondary_nav); ?>
-            <?php endif; ?>
-
-          </nav>
-        </div>
+<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+  <div class="container">
+    <div class="navbar-header">
+      <?php if ($logo): ?>
+      <a class="logo pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+      </a>
       <?php endif; ?>
-   </div>
-   </div>
+
+      <?php if (!empty($site_name)): ?>
+      <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+      <?php endif; ?>
+
+      <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <?php if (!empty($primary_nav)): ?>
+      <div class="navbar-collapse collapse">
+        <nav role="navigation">
+          <?php if (!empty($primary_nav)): ?>
+            <?php print render($primary_nav); ?>
+          <?php endif; ?>
+        </nav>
+      </div>
+    <?php endif; ?>
+  </div>
 </header>
 
 
